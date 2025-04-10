a05e39a8613bb46661e3ecc19fcaa51d24b73da3
Nava <navajay2018@gmail.com>
2025-04-10 14:34:48 +0330

    add field 'age' to human

--- src/db/example/Human.java
+++ src/db/example/Human.java
@@ -4,8 +4,10 @@ import db.Entity;
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 }
