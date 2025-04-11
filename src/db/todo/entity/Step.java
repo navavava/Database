b5b2dba5b8b0b561083e612cefc9b7647400e501
Nava <navajay2018@gmail.com>
2025-04-11 19:31:17 +0330

    create class Step and its contents

---
+++ src/db/todo/entity/Step.java
@@ -0,0 +1,26 @@
+package db.todo.entity;
+
+import db.Entity;
+
+public class Step extends Entity {
+
+    public static final int STEP_ENTITY_CODE = 2;
+    public String title;
+    public Status status;
+    public int taskRef;
+
+    public enum Status{
+        NotStarted,
+        Completed;
+    }
+
+    public Step(String title, Status status){
+        this.title = title;
+        this.status = status;
+    }
+    @Override
+    public int getEntityCode() {
+        return STEP_ENTITY_CODE;
+    }
+
+}
